e03634dcc65fc10f979658eaec259de7b6f6aae6
ashutosh tyagi <ashutosh.tyagi@hotmail.com>
2021-03-14 16:58:24 +0530

    First Thymleaf Template

--- src/main/java/com/ashcintosh/demo/web/RoomReservationWebController.java
+++ src/main/java/com/ashcintosh/demo/web/RoomReservationWebController.java
@@ -1,16 +1,16 @@
 package com.ashcintosh.demo.web;
 
-import java.util.Date;
-import java.util.List;
 
 import com.ashcintosh.demo.business.domain.RoomReservation;
 import com.ashcintosh.demo.business.service.ReservationService;
 
+import java.util.Date;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -24,12 +24,10 @@ public class RoomReservationWebController {
     }
 
     @GetMapping
-    public String getReservations(@RequestParam(value = "date", required = false) String dateString, Model model) {
-        Date date = DateUtils.createDateFromString(dateString);
-        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationForDate(date);
-        model.addAttribute("roomReservation", roomReservations);
+    public String getReservations(@RequestParam(value="date", required = false)String dateString, Model model){
+        Date date = DateUtils.createDateFromDateString(dateString);
+        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservations);
         return "reservations";
-
     }
-
 }
